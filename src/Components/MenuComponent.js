43869f76d6cb39be1dc21d3bab37f8e6d8162ec3
impulse <aman1122singh.as@gmail.com>
2020-07-11 21:25:21 +0530

    Header and Footer

--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 function MenuItem({ dish, onClick }) {
     return ( <
