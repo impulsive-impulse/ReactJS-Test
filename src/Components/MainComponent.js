85d18304cf24807a69997112cb4a37fc0ffcbe36
impulse <aman1122singh.as@gmail.com>
2020-07-12 21:05:45 +0530

    SPA part2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -9,6 +9,8 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import DishDetail from './Dishdetailcomponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -32,6 +34,21 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({ match }) => {
+            return ( <
+                DishDetail dish = { this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0] }
+                comments = { this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10)) }
+                />
+            );
+        }
+
+        const AboutUs = () => {
+            return ( <
+                About leaders = { this.state.leaders }
+                />
+            );
+        }
+
         return ( <
             div >
             <
@@ -47,8 +64,14 @@ class Main extends Component {
                 () => < Menu dishes = { this.state.dishes }
                 />} / >
                 <
+                Route path = '/menu/:dishId'
+                component = { DishWithId }
+                />  <
                 Route exact path = '/contactus'
                 component = { Contact }
+                /> <
+                Route path = '/aboutus'
+                component = { AboutUs }
                 /> <
                 Redirect to = "/home" / >
                 <
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function MenuItem({ dish }) {
     return ( <
         Card >
+        <
+        Link to = { `/menu/${dish.id}` } >
         <
         CardImg width = "100%"
         src = { dish.image }
@@ -13,6 +16,7 @@ function MenuItem({ dish }) {
         <
         CardTitle > { dish.name } < /CardTitle>  <
         /CardImgOverlay> <
+        /Link> <
         /Card> 
     );
 }
@@ -32,6 +36,21 @@ const Menu = (props) => {
     return ( <
         div className = "container" >
         <
+        div className = "row" >
+        <
+        Breadcrumb >
+        <
+        BreadcrumbItem > < Link to = "/home" > Home < /Link></BreadcrumbItem >
+        <
+        BreadcrumbItem active > Menu < /BreadcrumbItem> <
+        /Breadcrumb> <
+        div className = "col-12" >
+        <
+        h3 > Menu < /h3> <
+        hr / >
+        <
+        /div>                 <
+        /div> <
         div className = "row" > { menu } <
         /div> <
         /div>
