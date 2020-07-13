b13361f54de34fc8850dc7b407dc3c7305afdf8f
impulse <aman1122singh.as@gmail.com>
2020-07-13 23:33:20 +0530

    Form Validation

--- src/Components/ContactComponent.js
+++ src/Components/ContactComponent.js
@@ -17,6 +17,12 @@ class Contact extends Component {
     }
 
     render() {
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        const isNumber = (val) => !isNaN(Number(val));
+        const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
         return ( <
             div className = "container" >
             <
@@ -105,9 +111,26 @@ class Contact extends Component {
             id = "firstname"
             name = "firstname"
             placeholder = "First Name"
-            className = "form-control" /
-            >
-            <
+            className = "form-control"
+            validators = {
+                {
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15)
+                }
+            }
+            /> <
+            Errors className = "text-danger"
+            model = ".firstname"
+            show = "touched"
+            messages = {
+                {
+                    required: 'Required',
+                    minLength: 'Must be greater than 2 characters',
+                    maxLength: 'Must be 15 characters or less'
+                }
+            }
+            /> <
             /Col> <
             /Row> <
             Row className = "form-group" >
@@ -120,9 +143,26 @@ class Contact extends Component {
             id = "lastname"
             name = "lastname"
             placeholder = "Last Name"
-            className = "form-control" /
-            >
-            <
+            className = "form-control"
+            validators = {
+                {
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15)
+                }
+            }
+            /> <
+            Errors className = "text-danger"
+            model = ".lastname"
+            show = "touched"
+            messages = {
+                {
+                    required: 'Required',
+                    minLength: 'Must be greater than 2 characters',
+                    maxLength: 'Must be 15 characters or less'
+                }
+            }
+            /> <
             /Col> <
             /Row> <
             Row className = "form-group" >
@@ -135,9 +175,28 @@ class Contact extends Component {
             id = "telnum"
             name = "telnum"
             placeholder = "Tel. Number"
-            className = "form-control" /
-            >
-            <
+            className = "form-control"
+            validators = {
+                {
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                    isNumber
+                }
+            }
+            /> <
+            Errors className = "text-danger"
+            model = ".telnum"
+            show = "touched"
+            messages = {
+                {
+                    required: 'Required',
+                    minLength: 'Must be greater than 2 numbers',
+                    maxLength: 'Must be 15 numbers or less',
+                    isNumber: 'Must be a number'
+                }
+            }
+            />  <
             /Col> <
             /Row> <
             Row className = "form-group" >
@@ -150,8 +209,24 @@ class Contact extends Component {
             id = "email"
             name = "email"
             placeholder = "Email"
-            className = "form-control" / >
-            <
+            className = "form-control"
+            validators = {
+                {
+                    required,
+                    validEmail
+                }
+            }
+            /> <
+            Errors className = "text-danger"
+            model = ".email"
+            show = "touched"
+            messages = {
+                {
+                    required: 'Required',
+                    validEmail: 'Invalid Email Address'
+                }
+            }
+            /> <
             /Col> <
             /Row> <
             Row className = "form-group" >
